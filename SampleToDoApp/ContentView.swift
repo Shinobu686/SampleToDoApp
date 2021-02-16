b30033b038c1df5939f3d2acf155e56a8d37c35d
Shinobu686 <choppu.business@gmail.com>
2021-02-16 17:32:18 +0900

    first commit

---
+++ SampleToDoApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SampleToDoApp
+//
+//  Created by 久富稜也 on 2021/02/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}

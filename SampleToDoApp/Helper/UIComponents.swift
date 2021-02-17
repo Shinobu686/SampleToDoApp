589e25d668c1a41065884690a39a023362b34017
Shinobu686 <choppu.business@gmail.com>
2021-02-17 15:26:53 +0900

    fix #2 ヘルパークラスの作成

---
+++ SampleToDoApp/Helper/UIComponents.swift
@@ -0,0 +1,27 @@
+//
+//  Helper.swift
+//  SampleToDoApp
+//
+//  Created by 久富稜也 on 2021/02/17.
+//
+
+import Foundation
+import UIKit
+import SwiftUI
+
+struct UIComponents {
+    
+    // デバイス幅
+    static let screenWidth = UIScreen.main.bounds.width
+    
+    // デバイスの高さ
+    static let screenHeight = UIScreen.main.bounds.height
+    
+    //ナビゲーションバー色指定
+    static func setupNavigationBar() {
+        UINavigationBar.appearance().tintColor = .systemGray6
+        UINavigationBar.appearance().barTintColor = .systemTeal
+        UINavigationBar.appearance().titleTextAttributes = [.foregroundColor: UIColor.white]
+    }
+    
+}

636fb5d33cb28c5dcc2abbf0129ad4b93545e6bf
Shinobu686 <choppu.business@gmail.com>
2021-02-17 20:56:27 +0900

    fix #6 ToDoLIstViewの作成

--- SampleToDoApp/View/MainToDoView.swift
+++ SampleToDoApp/View/MainToDoView.swift
@@ -17,27 +17,37 @@ struct MainToDoView: View {
     
     var body: some View {
         NavigationView {
-            HStack {
-                TextField("キーワードを入力", text: $searchWord)
-                    .textFieldStyle(RoundedBorderTextFieldStyle())
-                    .frame(width: UIComponents.screenWidth / 1.4)
+            VStack {
+                HStack {
+                    TextField("キーワードを入力", text: $searchWord)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .frame(width: UIComponents.screenWidth / 1.4)
+                    
+                    Button(action:{
+                        //処理
+                    }) {
+                        Text("検索")
+                            .frame(width: UIComponents.screenWidth / 6, height: UIComponents.screenWidth / 13)
+                            .foregroundColor(.white)
+                            .background(Color(#colorLiteral(red: 0.1764705926, green: 0.4980392158, blue: 0.7568627596, alpha: 1)))
+                            .cornerRadius(10)
+                    }
+                }.padding(.top, 30)
+                .navigationBarTitle("やること", displayMode: .inline)
+                .navigationBarItems(trailing: Button(action:{
+                    
+                }){
+                    Text("編集")
+                })
                 
-                Button(action:{
-                    //処理
-                }) {
-                    Text("検索")
-                        .frame(width: UIComponents.screenWidth / 6, height: UIComponents.screenWidth / 13)
-                        .foregroundColor(.white)
-                        .background(Color(#colorLiteral(red: 0.1764705926, green: 0.4980392158, blue: 0.7568627596, alpha: 1)))
-                        .cornerRadius(10)
-                }
+                VStack(alignment: .leading,spacing: 1) {
+                    Text("ToDo一覧")
+                        .font(.largeTitle)
+                        .padding()
+                    
+                    ToDoListView()
+                }.padding(.top)
             }
-            .navigationBarTitle("やること", displayMode: .inline)
-            .navigationBarItems(trailing: Button(action:{
-                
-            }){
-                Text("編集")
-            })
         }
     }
 }
